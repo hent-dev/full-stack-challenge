3e59b43d26bcae43f0edfb65a3fea783d2a4f680
kinhosz <jcsc@cin.ufpe.br>
2021-11-20 00:49:24 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  root to: 'lends#index'
+  root to: 'books#index'
 
   resources :lends
   resources :books
-  resources :users
   devise_for :users
+  resources :users
 
   devise_scope :user do
     authenticated :user do
